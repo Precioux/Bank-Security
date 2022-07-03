9d5cad4f90938dc04cb82553293d4f7b00b7e9ce
Precioux <uni.mahdipour@gmail.com>
2022-07-04 01:57:31 +0430

    client format is changed

--- Client/Client/Client.ino
+++ Client/Client/Client.ino
@@ -3,9 +3,9 @@
 
 // setup LCD1
 const int rs = 35, en1 = 36, d4 = 40, d5 = 41, d6 = 42, d7 =43;
-// setup LCD1
+// setup LCD2
 const int  en2 = 37;
-// setup LCD1
+// setup LCD3
 const int  en3 = 38;
 bool doors[]={false,false,false};
 int rooms[]={0,0,0};
@@ -19,7 +19,6 @@ LiquidCrystal lcd3(rs, en3, d4, d5, d6, d7);
   bool getInput=false;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   lcd1.begin(20, 4);
   lcd1.clear();
@@ -33,7 +32,6 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
     if (Serial.available())                        // checking if data is received/available 
   {
            char input = Serial.read();    
@@ -80,111 +78,58 @@ void loop() {
   }
 
 void addRoom(int room){
-  lcd1.clear();
-  lcd2.clear();
-  lcd3.clear();
  rooms[room-1]++;
  doors[room-1]=true;
- switch(room)
- {
-  case 1:
-        lcd1.print("Door is open");
-        lcd1.setCursor(0, 1);
-        lcd1.print(rooms[room-1]);
-        lcd1.print(" person");
-        break;
-  case 2:
-        lcd2.print("Door is open");
-        lcd2.setCursor(0, 1);
-        lcd2.print(rooms[room-1]);
-        lcd2.print(" person");
-        break;
-  case 3:
-        lcd3.print("Door is open");
-        lcd3.setCursor(0, 1);
-        lcd3.print(rooms[room-1]);
-        lcd3.print(" person");
-        break;  
- }
+ showStatus();
 }
 
 void subRoom(int room)
 {
-  lcd1.clear();
-  lcd2.clear();
-  lcd3.clear();
  rooms[room-1]--;
- switch(room)
- {
-  case 1:
-        lcd1.print("Door is open");
-        lcd1.setCursor(0, 1);
-        lcd1.print(rooms[room-1]);
-        lcd1.print(" person");
-        break;
-  case 2:
-        lcd2.print("Door is open");
-        lcd2.setCursor(0, 1);
-        lcd2.print(rooms[room-1]);
-        lcd2.print(" person");
-        break;
-  case 3:
-        lcd3.print("Door is open");
-        lcd3.setCursor(0, 1);
-        lcd3.print(rooms[room-1]);
-        lcd3.print(" person");
-        break;  
- }
+ showStatus();
 }
 
 void lockRoom(int room)
 {
-  lcd1.clear();
-  lcd2.clear();
-  lcd3.clear();
   doors[room-1]=false;
- switch(room)
- {
-  case 1:
-        if(doors[room-1]==true){
-        lcd1.print("Door is open");
-        lcd1.setCursor(0, 1);
-        lcd1.print(rooms[room-1]);
-        lcd1.print(" person");}
-        else 
-          lcd1.print("Locked");
-        break;
-  case 2:
-        if(doors[room-1]==true){
-        lcd2.print("Door is open");
-        lcd2.setCursor(0, 1);
-        lcd2.print(rooms[room-1]);
-        lcd2.print(" person");}
-        else 
-          lcd1.print("Locked");
-        break;
-  case 3:
-        if(doors[room-1]==true){
-        lcd3.print("Door is open");
-        lcd3.setCursor(0, 1);
-        lcd3.print(rooms[room-1]);
-        lcd3.print(" person");}
-        else 
-          lcd1.print("Locked");
-        break; 
- }
+  showStatus();  
 }
 void emptyAll(){
-  lcd1.clear();
-  lcd2.clear();
-  lcd3.clear();
   rooms[0]=0;
   rooms[1]=0;
   rooms[2]=0;
   doors[0]=false;
   doors[1]=false;
   doors[2]=false;
-  lcd1.print("Locked and Empty");
-  lcd2.print("Locked and Empty");
-  lcd3.print("Locked and Empty");
+  showStatus();
+}
+void showStatus()
+{
+  lcd1.clear();
+  lcd2.clear();
+  lcd3.clear();
+  if(doors[0]==true)
+  {
+        lcd1.print("Door is open");
+        lcd1.setCursor(0, 1);
+        lcd1.print(rooms[0]);
+        lcd1.print(" person");
+  }
+  else  lcd1.print("Locked");
+  if(doors[1]==true)
+  {
+        lcd2.print("Door is open");
+        lcd2.setCursor(0, 1);
+        lcd2.print(rooms[1]);
+        lcd2.print(" person");
+  }
+  else  lcd2.print("Locked");
+  if(doors[2]==true)
+  {
+        lcd3.print("Door is open");
+        lcd3.setCursor(0, 1);
+        lcd3.print(rooms[2]);
+        lcd3.print(" person");
+  }
+  else  lcd3.print("Locked");  
 }
